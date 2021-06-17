330a836f8f3b6b6c8e12d3a0f9b0afb852c3a130
giselasanggaria <58310722+giselasanggaria@users.noreply.github.com>
2021-06-17 14:00:54 +0700

    Add files via upload

---
+++ StrukturdataTugas2/R 2.4.py
@@ -0,0 +1,28 @@
+class Flower:
+    def __init__(self, name, petals, price):
+        self._name = name
+        self._petals = petals
+        self._price = price
+
+    def get_name(self):
+        return self._name
+
+    def get_petals(self):
+        return self._petals
+
+    def get_price(self):
+        return self._price
+
+    def set_name(self, name):
+        self._name = name
+
+    def set_petals(self, petals):
+        self._petals = petals
+
+    def set_price(self, price):
+        self._price = price
+
+f = Flower('Kamboja', 24, 1.25)
+print(f.get_name())
+print(f.get_petals())
+print(f.get_price())
